4e80cd422d068ecb110d8e5f86faa89a3602993a
Wenjiu Wang <vanjor1014@gmail.com>
2020-09-18 01:15:08 -0400

    [CODE] Added functionality to match the test

---
+++ src/main/java/com/wenjiuwang/PiratenKapern/Player.java
@@ -0,0 +1,75 @@
+package com.wenjiuwang.PiratenKapern;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import sun.java2d.windows.GDIBlitLoops;
+
+public class Player {
+	int id;
+	String name;
+	int[] dice;
+	int score = 0;
+	int turnScore = 0;
+	Fortune fortune = Fortune.NONE;
+	int fortuneIndicator = 0;
+	boolean [] treasureChest = { false, false, false, false, false, false, false, false };
+
+	/*
+	 * Constructor
+	 */
+	public Player(String s) {
+		this.name = s;
+	}
+	
+	/*
+	 * Interface
+	 */
+	public void printInfo() {
+		System.out.println("|------------------------------------------------------------|");
+		System.out.println("|                                                            |");
+		System.out.println("| PLAYER: " + this.name + " | Total Score: " + this.score);
+		System.out.println("|                                                            |");
+		System.out.println("|------------------------------------------------------------|");
+		System.out.println("|                                                            |");
+		System.out.println("| Fortune Card Active: " + this.fortune.name() + (this.fortuneIndicator == 0 ? "" : " - " + this.fortuneIndicator));
+		System.out.println("|                                                            |");
+		System.out.println("|------------------------------------------------------------|");
+	}
+
+	public void printDice() {
+		System.out.println("|------------------------------------------------------------|");
+		System.out.println("| Current Dice Rolled:                                       |");
+		System.out.println("|                                                            |");
+		System.out.println("| 1: " + Object.values()[this.dice[0]-1].name() + " | 2: " + Object.values()[this.dice[1]-1].name() + " | 3: " + Object.values()[this.dice[2]-1].name() + " | 4:  " + Object.values()[this.dice[3]-1].name());
+		System.out.println("|                                                            |");
+		System.out.println("| 5: " + Object.values()[this.dice[4]-1].name() + " | 6: " + Object.values()[this.dice[5]-1].name() + " | 7: " + Object.values()[this.dice[6]-1].name() + " | 8:  " + Object.values()[this.dice[7]-1].name());
+		System.out.println("|                                                            |");
+		System.out.println("| Current Rolled Score: " + this.turnScore);
+		System.out.println("|------------------------------------------------------------|");
+		if (this.fortune == Fortune.TREASURECHEST) printChest();
+	}
+	
+	public void printChest() {
+		System.out.println("|------------------------------------------------------------|");
+		System.out.println("| Current Dice in Treasure Chest:                            |");
+		System.out.println("|                                                            |");
+		for (int i = 0; i < this.treasureChest.length; ++i) {
+			if (this.treasureChest[i]) {
+				System.out.print("| " + (i+1) + ": " + Object.values()[this.dice[i]-1].name() + " ");
+			}
+		}
+		System.out.println("|                                                            |");
+		System.out.println("|------------------------------------------------------------|");
+
+	}
+	
+
+
+}
