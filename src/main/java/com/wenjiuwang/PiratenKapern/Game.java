56dab32ac92bc883157ba5ffcf305cf628d1a101
Wenjiu Wang <vanjor1014@gmail.com>
2020-09-18 01:53:08 -0400

    [CODE] (Networking, no test needed) Added functionality for server side normal turn message sending and receiving

--- src/main/java/com/wenjiuwang/PiratenKapern/Game.java
+++ src/main/java/com/wenjiuwang/PiratenKapern/Game.java
@@ -387,20 +387,70 @@ public class Game {
 		        while(playTurn) {
 					Gamedata gd = (Gamedata) this.clients[playerNum].inStream.readObject();
 					switch(gd.code) {
-						case REROLL:
-							//re roll
-							break;
-						case PUT_IN:
-							//Put dice in the chest 
-							break;
-						case TAKE_OUT:
-							//Take dice out of the chest
-							break;
+					case REROLL:
+						//re roll
+						int[] pos = gd.data;
+						curDice = this.reroll(curDice, pos);
+						this.sendRequest(playerNum, (skulls >= 4 && this.fortune != Fortune.SEABATTLE ? RequestCode.ISLAND_OF_SKULLS : RequestCode.NORMAL), curDice, 
+								(skulls >= 4 && this.fortune != Fortune.SEABATTLE ? this.turnDeductScore(curDice) : this.turnTotalScore(curDice)));
+						break;
+					case PUT_IN:
+						//Put dice in the chest 
+						for (int i = 0; i < gd.data.length; ++i) {
+							if (this.treasureChest[gd.data[i]-1]) {
+								System.out.println("Skip die #" + gd.data[i] + " cause it is already in the chest.");
+							} else {
+								this.treasureChest[gd.data[i]-1] = true;
+							}
+						}
+						this.sendRequest(playerNum, RequestCode.NORMAL, curDice, this.turnTotalScore(curDice));
+						break;
+					case TAKE_OUT:
+						//Take dice out of the chest
+						for (int i = 0; i < gd.data.length; ++i) {
+							if (this.treasureChest[gd.data[i]-1]) {
+								this.treasureChest[gd.data[i]-1] = false;
+							} else {
+								System.out.println("Skip die #" + gd.data[i] + " cause it not in the chest.");
+							}
+						}
+						this.sendRequest(playerNum, RequestCode.NORMAL, curDice, this.turnTotalScore(curDice));
+						break;
 						default:
 							playTurn = false;
 							break;
 					}
-		        }			
+		        }
+				//update player points:
+		        if (skulls >= 4 && this.fortune != Fortune.SEABATTLE) {
+		        	//island of skulls
+					for (int i = 0; i < 3; ++i) {
+						 int[] info = { playerNum };
+						if (i != playerNum) {
+							this.players[i].score -= this.turnDeductScore(curDice);
+							this.sendRequest(i, RequestCode.DEDUCT, info, this.turnDeductScore(curDice));
+						}
+					}
+		        } else {
+		        	//NORMAL TURN
+					this.players[playerNum].score += this.turnTotalScore(curDice);
+		        }
+		        
+				//check if reach 6000 points
+				if (players[playerNum].score >= 6000 || finalRounds <= 1) {
+					finalRounds -= 1;
+				}
+				
+				//determine next player
+				playerNum = (playerNum < 2) ? playerNum + 1 : 0;
+				
+				//clear treasure chest
+				for (int i = 0; i < this.treasureChest.length; ++i) {
+					this.treasureChest[i] = false;
+				}
+				
+				//clear fortune num
+				this.fortuneIndicator = 0;
 			}
 		} catch (IOException ex) {
 			System.out.println("Disconnected");
