c9af962f9458b79e5c495280dd61fc01ad3a7040
Wenjiu Wang <vanjor1014@gmail.com>
2020-09-17 21:47:40 -0400

    - setup project

---
+++ src/main/java/com/wenjiuwang/PiratenKapern/App.java
@@ -0,0 +1,13 @@
+package com.wenjiuwang.PiratenKapern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
